7562a26f5324ca4f83998d456afa6d2a141f3a41
Christianekenstedt <Christian@n158-p204.kthopen.kth.se>
2015-09-23 10:52:39 +0200

    New menu

--- Laboration3b/src/laboration3b/UserInterface.java
+++ Laboration3b/src/laboration3b/UserInterface.java
@@ -1,5 +1,5 @@
 package laboration3b;
-
+import java.util.Scanner;
 /**
  *
  * @author Chrille
@@ -7,15 +7,53 @@ package laboration3b;
 public class UserInterface {
     
     public UserInterface(){
-        
+        menu();
     }
     public void menu(){
-        
+        run();
     }
     public void addBook(){
         
     }
     public void getBooksByTitle(String title){
         
+    }
+    
+    private void run(){
+        Scanner scan = new Scanner(System.in);
+        char choice;
+    	String answer;
+    	
+    	do {
+    		printMenu();
+    		answer = scan.nextLine();
+    		answer = answer.toUpperCase();
+    		choice = answer.charAt(0); // Fˆrsta tecknet i svaret
+    		
+    		switch(choice) {
+    			case 'A':	doThis(); break;
+    			case 'B':	doThat(); break;
+    			case 'X':	System.out.println("Bye, bye!"); break;
+    			default: 	System.out.println("Unknown command");
+    		}
+    		
+    	} while(choice != 'X');
+    }
+    // Definierar ett "uppdrag"
+    private void doThis() {
+    	System.out.println("You choose A");
+    }
+    
+    // Definierar ett annat "uppdrag"
+    private void doThat() {
+    	System.out.println("You choose B");
+    }
+    
+    private void printMenu() {
+    	System.out.println("---Menu---");
+    	System.out.println("A Do This");
+    	System.out.println("B Do That");
+    	System.out.println("X Exit");
+    	System.out.println("----------");
     }
 }
