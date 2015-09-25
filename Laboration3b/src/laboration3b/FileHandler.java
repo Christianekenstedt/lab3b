8ab91e87c5a78887963730477e7a69f5416d85c4
Christianekenstedt <Christian@n134-p5.kthopen.kth.se>
2015-09-25 14:26:28 +0200

    Nu funkar det! nästa... blev något fel när vi söker efter Author på plats två om de finns flera

--- Laboration3b/src/laboration3b/FileHandler.java
+++ Laboration3b/src/laboration3b/FileHandler.java
@@ -4,45 +4,51 @@
  * and open the template in the editor.
  */
 package laboration3b;
+
 import java.io.*;
 import java.lang.*;
 import java.util.ArrayList;
+
 /**
  *
  * @author Christian
  */
 public class FileHandler {
-    
-    public ArrayList<Book> read() throws Exception{
-        
+
+    public ArrayList<Book> read() throws Exception {
+
         ObjectInputStream ois = null;
         ArrayList<Book> books;
-        try{
+        try {
             FileInputStream fin = new FileInputStream("booklist.ser");
             ois = new ObjectInputStream(fin);
-            
+
             books = (ArrayList<Book>) ois.readObject();
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot find file, creating new...");
             File file = new File("booklist.ser");
             file.createNewFile();
             return null;
-        }
-        finally{
-            if(ois != null) ois.close();
-            
+        } finally {
+            if (ois != null) {
+                ois.close();
+            }
+
         }
         return books;
     }
-    public void write(ArrayList<Book> books) throws Exception{
+
+    public void write(ArrayList<Book> books) throws Exception {
         ObjectOutputStream oos = null;
-        try{
+        try {
             FileOutputStream fout = new FileOutputStream("booklist.ser");
             oos = new ObjectOutputStream(fout);
             oos.writeObject(books);
-            
-            
-        }finally{
-            if(oos != null) oos.close();
-        }         
+
+        } finally {
+            if (oos != null) {
+                oos.close();
+            }
+        }
     }
 }
